37d83071968aec91ebe0ae853dec61ffe3bafba7
Andrey Byelogurov <byelogurov@gmail.com>
2015-02-11 11:07:41 -0800

    Not sure what im doing

---
+++ AsteroidsClone/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace AsteroidsClone {
+	class Player {
+		Texture2D Graphic;
+
+		public void OnLoad() {
+			Graphic = Content.Load<Texture2D>("Test/Player");
+		}
+	}
+}
